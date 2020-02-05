3e2a020cb8e6bceeceeaa0c662156f0b90439729
SELIG-C <carl_selig@hotmail.com>
2020-02-05 14:05:33 +0000

    Cleanup and ignore
    Cleaned up the repo structure and added the experimental file

---
+++ startup.m
@@ -0,0 +1,5 @@
+% Set the wecSimPath variable to the location of the WEC-Sim source directory
+% Copy the code in this file and place it in your'startup.m' file
+
+wecSimPath = "C:\Users\user\Documents\WECSim";
+addpath(genpath(wecSimPath));
