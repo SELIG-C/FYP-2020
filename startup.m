9e05e007e7cf52b17cea82747e57d4f492332693
SELIG-C <carl_selig@hotmail.com>
2020-02-10 14:25:38 +0000

    Daily 10/02/2020
    Daily commit

--- startup.m
+++ startup.m
@@ -1,5 +1,5 @@
 % Set the wecSimPath variable to the location of the WEC-Sim source directory
 % Copy the code in this file and place it in your'startup.m' file
 
-wecSimPath = "C:\Users\user\Documents\WECSim";
+wecSimPath = "C:\Users\user\Documents\FYP-2020";
 addpath(genpath(wecSimPath));
